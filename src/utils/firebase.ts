db802f8413e4eb83643ab423a86023e711ee80cc
happy663 <53854298+happy663@users.noreply.github.com>
2022-09-14 12:52:35 +0900

    簡単な環境構築

---
+++ src/utils/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUWDuGISmyKlTQrTFBtrdIFeXl-bEtk0Y",
+  authDomain: "hackday-4daec.firebaseapp.com",
+  projectId: "hackday-4daec",
+  storageBucket: "hackday-4daec.appspot.com",
+  messagingSenderId: "33897607847",
+  appId: "1:33897607847:web:5af4e2bdabc87c1bba6182",
+  measurementId: "G-M0GL6HX96J",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+const analytics = getAnalytics(app);
