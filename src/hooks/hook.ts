ba2a234c404608d0c3b6bdbdb54a025621548996
happy663 <53854298+happy663@users.noreply.github.com>
2022-09-15 07:27:00 +0900

    add: カスタムフック作成ロジックの分離

---
+++ src/hooks/hook.ts
@@ -0,0 +1,32 @@
+import { useRecoilState } from 'recoil';
+import { questionsState } from 'src/globalStates/atoms/questionsState';
+import React from 'react';
+import { Question } from 'src/types';
+import { collection, getDocs } from 'firebase/firestore';
+import { questionConverter } from 'src/utils/firebaseConverter';
+import { db } from 'src/utils/firebase';
+import { currentQuestionState } from 'src/globalStates/atoms';
+
+export const useQuestion = () => {
+  const [questions, setQuestions] = useRecoilState(questionsState);
+  const [currentQuestion, setCurrentQuestion] =
+    useRecoilState(currentQuestionState);
+
+  React.useEffect(() => {
+    const fetchAndSetQuestions = async () => {
+      const dataQuestions: Question[] = [];
+      const querySnapshot = await getDocs(
+        collection(db, 'Questions').withConverter(questionConverter)
+      );
+      querySnapshot.forEach((doc) => {
+        dataQuestions.push(doc.data());
+      });
+
+      setQuestions([...questions, ...dataQuestions]);
+      setCurrentQuestion(dataQuestions[0]);
+    };
+    fetchAndSetQuestions();
+  }, []);
+
+  return { questions, currentQuestion, setQuestions, setCurrentQuestion };
+};
