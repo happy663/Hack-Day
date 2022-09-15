db147978f4770c7d5aa4e713eb3cedbc091d5cc3
furukawaTakumi <black3moon88@gmail.com>
2022-09-15 23:22:21 +0900

    feat: 背景を作成

--- src/containers/NewQuestion.tsx
+++ src/containers/NewQuestion.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export const NewQuestion = () => {
   return (
-    <View style={{ flex: 1 }}>
+    <LinearGradient
+      style={styles.root}
+      colors={["#F4A261", "#E9C46A"]}
+      start={{ x: 0.5, y: 0.65 }}
+    >
       <Text>hellow new question</Text>
-    </View>
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    paddingTop: 45,
+  },
+});
