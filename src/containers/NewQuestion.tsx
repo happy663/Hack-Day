ea2f7950700c05af82bc2854e74dcc12b0815d8d
furukawaTakumi <black3moon88@gmail.com>
2022-09-15 23:46:20 +0900

    feat: 円を記述する

--- src/containers/NewQuestion.tsx
+++ src/containers/NewQuestion.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 export const NewQuestion = () => {
@@ -9,7 +9,7 @@ export const NewQuestion = () => {
       colors={["#F4A261", "#E9C46A"]}
       start={{ x: 0.5, y: 0.65 }}
     >
-      <Text>hellow new question</Text>
+      <View style={styles.microphone}></View>
     </LinearGradient>
   );
 };
@@ -18,5 +18,12 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     paddingTop: 45,
+  },
+  microphone: {
+    borderRadius: Dimensions.get("window").width / 2,
+    height: Dimensions.get("window").width,
+    backgroundColor: "#E76F51",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
