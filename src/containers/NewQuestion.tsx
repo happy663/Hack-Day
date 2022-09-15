b1a204d2429e9524f3ab24c51560b40aa4d5f35d
furukawaTakumi <black3moon88@gmail.com>
2022-09-16 00:12:25 +0900

    feat: 二つ目の丸

--- src/containers/NewQuestion.tsx
+++ src/containers/NewQuestion.tsx
@@ -9,20 +9,32 @@ export const NewQuestion = () => {
       colors={["#F4A261", "#E9C46A"]}
       start={{ x: 0.5, y: 0.65 }}
     >
-      <View style={styles.microphone}></View>
+      <View style={styles.outsideWave}>
+        <View style={styles.middlesideWave}></View>
+      </View>
     </LinearGradient>
   );
 };
+
+const screenWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
     paddingTop: 45,
   },
-  microphone: {
-    borderRadius: Dimensions.get("window").width / 2,
-    height: Dimensions.get("window").width,
-    backgroundColor: "#E76F51",
+  outsideWave: {
+    borderRadius: screenWidth / 2,
+    height: screenWidth,
+    backgroundColor: "#ed8858",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  middlesideWave: {
+    borderRadius: screenWidth / 2,
+    width: screenWidth / 1.5,
+    height: screenWidth / 1.5,
+    backgroundColor: "#e97553",
     justifyContent: "center",
     alignItems: "center",
   },
