e27ec444d4156fb3045941643d0d0799f5a4c3a1
happy663 <53854298+happy663@users.noreply.github.com>
2022-09-16 22:21:22 +0900

    update: question_idに一致するchatのみを取得

--- src/hooks/useChats.ts
+++ src/hooks/useChats.ts
@@ -1,21 +1,29 @@
-import { getDoc } from 'firebase/firestore';
+import { getDoc, query, where, collection, getDocs } from 'firebase/firestore';
 import { Chat } from 'src/types';
 import React, { useRef } from 'react';
-
-import { collection, getDocs } from 'firebase/firestore';
 import { db } from 'src/utils/firebase';
 import { chatConverter } from 'src/utils/firebaseConverter';
+import { useRecoilValue } from 'recoil';
+import { currentQuestionState } from 'src/globalStates/atoms';
 
 export const useChats = () => {
   const [chats, setChats] = React.useState<Chat[]>([]);
+  const currentQuestion = useRecoilValue(currentQuestionState);
   React.useEffect(() => {
     const fetchAndSetChats = async () => {
       const arrayChats: Chat[] = [];
       try {
-        const querySnapshot = await getDocs(
-          collection(db, 'Chats').withConverter(chatConverter)
+        console.log(currentQuestion.question_id);
+
+        const q = query(
+          collection(db, 'Chats').withConverter(chatConverter),
+          where('question_id', '==', currentQuestion.question_id)
         );
+        const querySnapshot = await getDocs(q);
+        console.log(querySnapshot);
+
         querySnapshot.forEach((doc) => {
+          console.log(doc.id, ' => ', doc.data());
           arrayChats.push(doc.data());
         });
         setChats(arrayChats);
@@ -24,7 +32,7 @@ export const useChats = () => {
       }
     };
     fetchAndSetChats();
-  }, []);
+  }, [currentQuestion]);
 
-  return {chats};
+  return { chats };
 };
