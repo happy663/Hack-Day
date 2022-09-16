2418bd9cbd6c3a18650bad5ec9bf2d2716c769b6
happy663 <53854298+happy663@users.noreply.github.com>
2022-09-17 00:13:32 +0900

    update: ログインページとコンテンツページで表示分岐

--- App.tsx
+++ App.tsx
@@ -1,29 +1,37 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import { RecoilRoot } from "recoil";
-import { PageRootDefine, AppNavigationRef } from "src/routes/ApplicationRoutes";
-import { Home, ChatsPage, NewQuestion } from "src/containers";
-import { Header, NavigationBar } from "src/components/layout";
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { RecoilRoot } from 'recoil';
+import { PageRootDefine, AppNavigationRef } from 'src/routes/ApplicationRoutes';
+import { Home, ChatsPage, NewQuestion } from 'src/containers';
+import { Header, NavigationBar } from 'src/components/layout';
+import { View } from 'react-native';
+import { LoginPages } from 'src/containers/LoginPage';
 
 const Tabs = createBottomTabNavigator<PageRootDefine>();
 
 export default function App() {
+  const isAuth = false;
+
   return (
     <RecoilRoot>
       <NavigationContainer ref={AppNavigationRef}>
         <Header />
-        <Tabs.Navigator
-          initialRouteName={"Home"}
-          tabBar={(props) => <NavigationBar {...props} />}
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Tabs.Screen name="Home" component={Home} />
-          <Tabs.Screen name="NewQuestion" component={NewQuestion} />
-          <Tabs.Screen name="ChatsPage" component={ChatsPage} />
-        </Tabs.Navigator>
+        {isAuth ? (
+          <Tabs.Navigator
+            initialRouteName={'Home'}
+            tabBar={(props) => <NavigationBar {...props} />}
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Tabs.Screen name="Home" component={Home} />
+            <Tabs.Screen name="NewQuestion" component={NewQuestion} />
+            <Tabs.Screen name="ChatsPage" component={ChatsPage} />
+          </Tabs.Navigator>
+        ) : (
+          <LoginPages />
+        )}
       </NavigationContainer>
     </RecoilRoot>
   );
