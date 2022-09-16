cab58f31a60a742937cacf07744cbe871117ee4a
furukawaTakumi <black3moon88@gmail.com>
2022-09-16 11:17:57 +0900

    feat:画面ステータスの宣言

--- src/containers/NewQuestion.tsx
+++ src/containers/NewQuestion.tsx
@@ -3,13 +3,25 @@ import { StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { theme } from "src/utils/theme";
 import { MicrophoneOnCircle, Prompt } from "src/components";
-import { useRecoilState } from "recoil";
-import { isRecordingState } from "src/globalStates/atoms/newQuestionState";
+import { useRecoilValue } from "recoil";
+import {
+  NewQuestionScreenState,
+  newQuestionState,
+} from "src/globalStates/atoms/newQuestionState";
 
 export interface NewQuestionProps {}
 
 export const NewQuestion = () => {
-  const [isRecording, _] = useRecoilState(isRecordingState);
+  const screenStatus = useRecoilValue(newQuestionState);
+
+  const promptDic: { [key in NewQuestionScreenState]: string } = {
+    ready: "ご相談はなんでしょうか？",
+    recording: "質問中",
+    recognizing: "文字起こし中",
+    SUCCESS: "完了しました",
+    NG_WORD: "汚い言葉は使えません",
+    TOO_SHORT_VOICE: "質問が短すぎます",
+  };
 
   return (
     <LinearGradient
@@ -18,7 +30,7 @@ export const NewQuestion = () => {
       start={{ x: 0.5, y: 0.65 }}
     >
       <MicrophoneOnCircle />
-      <Prompt>{isRecording ? "質問中……" : "ご相談はなんでしょうか？"}</Prompt>
+      <Prompt>{promptDic[screenStatus]}</Prompt>
     </LinearGradient>
   );
 };
--- src/globalStates/atoms/newQuestionState.ts
+++ src/globalStates/atoms/newQuestionState.ts
@@ -1,6 +1,24 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+
+export type RecordingResultType = "SUCCESS" | "NG_WORD" | "TOO_SHORT_VOICE";
 
 export const isRecordingState = atom({
   key: "isRecordingState",
   default: false,
+});
+
+export type NewQuestionScreenState =
+  | "ready"
+  | "recording"
+  | "recognizing"
+  | RecordingResultType;
+
+export const newQuestionState = selector<NewQuestionScreenState>({
+  key: "newQuestionState",
+  get: ({ get }) => {
+    const isRecording = get(isRecordingState);
+
+    if (isRecording) return "recording";
+    return "ready";
+  },
 });
