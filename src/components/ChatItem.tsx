066722c6240e67ee2123e98e759ba70e38ce339d
happy663 <53854298+happy663@users.noreply.github.com>
2022-09-18 06:48:33 +0900

    add: 画像を丸く

--- src/components/ChatItem.tsx
+++ src/components/ChatItem.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
-import { Player } from './Player';
-import { theme, globalStyles } from 'src/utils/theme';
-import { format } from 'date-fns';
-import { Chat } from 'src/types';
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { Player } from "./Player";
+import { theme, globalStyles } from "src/utils/theme";
+import { format } from "date-fns";
+import { Chat } from "src/types";
 
 export const ChatItem = ({ type, created_at, user, voice }: Chat) => {
   const alignItems =
-    type === 'questioner'
-      ? 'flex-end'
-      : type === 'respondent'
-      ? 'flex-start'
-      : 'center';
+    type === "questioner"
+      ? "flex-end"
+      : type === "respondent"
+      ? "flex-start"
+      : "center";
   const backgroundColor =
-    type === 'questioner'
-      ? '#AFE885'
-      : type === 'respondent'
-      ? '#BFD2D3'
-      : '#FF8F8F';
+    type === "questioner"
+      ? "#AFE885"
+      : type === "respondent"
+      ? "#BFD2D3"
+      : "#FF8F8F";
 
   const age = new Date().getFullYear() - user.birth_year;
 
@@ -25,15 +25,18 @@ export const ChatItem = ({ type, created_at, user, voice }: Chat) => {
     <View style={styles.container}>
       <View style={{ alignItems: alignItems }}>
         <View style={styles.profileContainer}>
-          <Image source={{ uri: user.icon_url }} style={globalStyles.iconSm} />
+          <Image
+            source={{ uri: user.icon_url }}
+            style={{ ...globalStyles.iconSm, borderRadius: 30 }}
+          />
           <View style={{ marginLeft: 8, alignItems: alignItems }}>
             <Text style={globalStyles.textBold}>
-              {type !== 'thank_you'
+              {type !== "thank_you"
                 ? `${user.name}さん (${user.gender} / ${age}歳)`
                 : `${user.name}さんのありがとう`}
             </Text>
-            {type !== 'thank_you' && (
-              <Text>{format(created_at, 'yyyy年MM月dd日')}</Text>
+            {type !== "thank_you" && (
+              <Text>{format(created_at, "yyyy年MM月dd日")}</Text>
             )}
           </View>
         </View>
@@ -55,11 +58,11 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   profileContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   chatContentContainer: {
     height: 96,
-    width: '95%',
+    width: "95%",
     borderRadius: 10,
     paddingVertical: 10,
     ...globalStyles.flexCenter,
