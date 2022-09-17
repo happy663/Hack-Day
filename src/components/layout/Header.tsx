1c96ac9cb65828ef101ea1928de90649aa1c889d
furukawaTakumi <black3moon88@gmail.com>
2022-09-18 08:10:19 +0900

    fix: ロゴが綺麗に映るように修正

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,18 @@
-import { ReactNode } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { theme, globalStyles } from "src/utils/theme";
 import Logo from "assets/Logo.svg";
 
 export const Header = () => {
   return (
     <View style={styles.header}>
-      <Logo width={120} height={40} />
+      <Logo width={200} height={100} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   header: {
-    height: 60,
+    height: 80,
     paddingTop: 20,
     backgroundColor: theme.colors.white,
     ...globalStyles.flexCenter,
