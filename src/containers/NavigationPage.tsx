7375b0d3975d048eb0edc88095de0af591a6a551
happy663 <53854298+happy663@users.noreply.github.com>
2022-09-18 02:22:16 +0900

    add:レイアウトゴミだけどUserForm画面作成

--- src/containers/NavigationPage.tsx
+++ src/containers/NavigationPage.tsx
@@ -1,36 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil';
-import { PageRootDefine, AppNavigationRef } from 'src/routes/ApplicationRoutes';
-import { Home, ChatsPage, NewQuestion } from 'src/containers';
-import { Header, NavigationBar } from 'src/components/layout';
-import { View } from 'react-native';
-import { LoginPages } from 'src/containers/LoginPage';
-import { useIsLogin } from 'src/hooks/useIsLogin';
-import { firebaseUserState } from 'src/globalStates/atoms/firebaseUserState';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { PageRootDefine, AppNavigationRef } from "src/routes/ApplicationRoutes";
+import { Home, ChatsPage, NewQuestion } from "src/containers";
+import { Header, NavigationBar } from "src/components/layout";
+import { Text, View } from "react-native";
+import { LoginPages } from "src/containers/LoginPage";
+import { useIsLogin } from "src/hooks/useIsLogin";
+import { firebaseUserState } from "src/globalStates/atoms/firebaseUserState";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { UserFirstSetUpPage } from "src/containers/UserFirstSetUpPage";
 
 const Tabs = createBottomTabNavigator<PageRootDefine>();
 
 export const NavigationPage = () => {
   const isLogin = useIsLogin();
+  const existingUser = false;
 
   return (
     <NavigationContainer ref={AppNavigationRef}>
       <Header />
       {isLogin ? (
-        <Tabs.Navigator
-          initialRouteName={'Home'}
-          tabBar={(props) => <NavigationBar {...props} />}
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Tabs.Screen name="Home" component={Home} />
-          <Tabs.Screen name="NewQuestion" component={NewQuestion} />
-          <Tabs.Screen name="ChatsPage" component={ChatsPage} />
-        </Tabs.Navigator>
+        existingUser ? (
+          //既存ユーザなら
+          <Tabs.Navigator
+            initialRouteName={"Home"}
+            tabBar={(props) => <NavigationBar {...props} />}
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Tabs.Screen name="Home" component={Home} />
+            <Tabs.Screen name="NewQuestion" component={NewQuestion} />
+            <Tabs.Screen name="ChatsPage" component={ChatsPage} />
+          </Tabs.Navigator>
+        ) : (
+          //新規ユーザなら
+          <UserFirstSetUpPage />
+        )
       ) : (
         <LoginPages />
       )}
---
+++ src/containers/UserFirstSetUpPage.tsx
@@ -0,0 +1,150 @@
+import { Picker } from "@react-native-picker/picker";
+import React from "react";
+import {
+  Button,
+  View,
+  TextInput,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { useRecoilValue } from "recoil";
+import { firebaseUserState } from "src/globalStates/atoms/firebaseUserState";
+import { theme } from "src/utils/theme";
+
+export const UserFirstSetUpPage = () => {
+  const [name, setName] = React.useState("");
+  const [gender, setGender] = React.useState("男性");
+  const [birthYear, setBirthYear] = React.useState(2000);
+  const [introduciton, setIntroduciton] =
+    React.useState("よろしくお願いします");
+
+  console.log(name);
+  console.log(gender);
+  console.log(birthYear);
+  console.log(introduciton);
+
+  const years = [
+    1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961,
+    1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973,
+    1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985,
+    1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997,
+    1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
+    2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
+  ];
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          marginLeft: 12,
+        }}
+      >
+        ユーザー名
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setName}
+        value={name}
+        placeholder=""
+      />
+      <Text
+        style={{
+          marginLeft: 12,
+        }}
+      >
+        性別
+      </Text>
+      <Picker
+        selectedValue={gender}
+        onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
+        style={{
+          height: 150,
+          width: 150,
+        }}
+      >
+        <Picker.Item label="男性" value="男性" />
+        <Picker.Item label="女性" value="女性" />
+      </Picker>
+
+      <Text
+        style={{
+          marginLeft: 12,
+        }}
+      >
+        誕生年
+      </Text>
+      <Picker
+        selectedValue={birthYear}
+        onValueChange={(itemValue, itemIndex) => setBirthYear(itemValue)}
+        style={{
+          height: 150,
+          width: 150,
+        }}
+      >
+        {years.map((year, index) => {
+          return (
+            <Picker.Item
+              key={index}
+              label={year.toString()}
+              value={year.toString()}
+            />
+          );
+        })}
+      </Picker>
+      <Text
+        style={{
+          marginLeft: 12,
+        }}
+      >
+        自己紹介
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setIntroduciton}
+        value={introduciton}
+      />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <TouchableOpacity style={styles.submitButton} onPress={() => {}}>
+          <Text
+            style={{
+              fontSize: 24,
+            }}
+          >
+            登録
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "black",
+  },
+  submitButton: {
+    flexGrow: 0.4,
+    height: 48,
+    justifyContent: "center",
+    flexDirection: "row",
+    borderRadius: 16,
+    alignItems: "center",
+    backgroundColor: theme.colors.main,
+    marginTop: 24,
+  },
+});
